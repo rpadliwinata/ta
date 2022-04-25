e3074cd76c0e8d22d9b071fdc2c5b0b4f1b08987
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-04-25 10:41:57 +0700

    added write file

--- get_captcha.py
+++ get_captcha.py
@@ -32,17 +32,7 @@ if r.status_code == 200:
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
 
 img = cv2.imread("../image.jpg")
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# ret, thresh1 = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)
-# rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
-# dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
-# contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-# im2 = img.copy()
-# x, y, w, h = cv2.boundingRect(contours[0])
-# rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-# cropped = im2[y:y + h, x:x + w]
 ret, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-#cropped = th1
 
 captcha = pytesseract.image_to_string(th1)
 captcha = captcha.strip()
@@ -57,5 +47,8 @@ res = session.post(
     "http://wbz2lrxhw4dd7h5t2wnoczmcz5snjpym4pr7dzjmah4vi6yywn37bdyd.onion/login.php", data=data, headers=headers)
 
 cookies = session.cookies.get_dict()
+with open("cookies_list.txt", "w") as file:
+    for cookie in cookies.values:
+        file.write(cookie)
 
 
