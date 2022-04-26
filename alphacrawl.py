309ef65514e679d24e0055f99eeb3ae73df1dfba
FinaFahrani <finafahrani@student.telkomuniversity.ac.id>
2022-04-26 10:39:14 +0700

    .

---
+++ alphacrawl.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import argparse
+from traceback import print_tb
+# TorCrawl Modules
+from modules.crawler import crawler
+from modules.checker import *
+
+help = '''
+Umum:
+-h, --help         : Bantuan
+-u, --url *.onion  : Untuk menentukan URL awal yang akan di crawl 
+
+Crawler:
+-d, --cdepth      : Mengatur tingkat kedalaman crawl (Default: 1)
+-p, --cpause      : Untuk mengatur lama waktu pause saat crawling (Default: 0)
+'''
+
+
+
+
+def main():
+	# inisialisasi nilai default  variabel
+	cdepth = 1
+	cpause = 0
+
+	parser = argparse.ArgumentParser(
+		description="AlphaCrawl.py adalah crawler berbahasa python yang dapat digunakan untuk melakukan crawling pada Darkweb.")
+
+	parser.add_argument(
+		'-u',
+		'--url',
+		help='Untuk menentukan URL awal yang akan di crawl'
+	)
+	parser.add_argument(
+		'-d',
+		'--cdepth',
+		help='Mengatur tingkat kedalaman crawl (Default: 1)'
+	)
+	parser.add_argument(
+        '-p',
+        '--cpause',
+        help='Untuk mengatur lama waktu pause saat crawling (Default: 0)'
+    )
+
+	args = parser.parse_args()
+
+	# mengubah argumen parser menjadi variabel
+	if args.cdepth:
+		cdepth = args.cdepth
+	if args.cpause:
+		cpause = args.cpause
+
+	# Cek Layanan TOR
+	checktor()
+
+	# Perbaikan link, pembuatan outpath dan pemanggilan proses crawling
+	if len(args.url) > 0:
+		global website
+		global outpath
+		website = urlcanon(args.url)
+		outpath = folder(extract_domain(website))
+		crawler(website, cdepth, cpause, outpath)
+		print(("\n## File hasil berada pada : " + os.getcwd() + "/" + outpath))
+		print("\n\n** Proses Crawling Selesai **\n")
+
+if __name__ == "__main__":
+    main()
---
+++ modules/checker.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys
+import re
+import subprocess
+import os
+from urllib.parse import urlparse
+
+#Pengecekan dan/ Perbaikan link input
+def urlcanon(website):
+	if not website.startswith("http"):
+		if not website.startswith("www."):
+			website = "www." + website
+		website = "http://" + website
+		print(("## URL diperbaiki : " + website))
+	return website
+
+
+def extract_domain(url, remove_http=True):
+	uri = urlparse(url)
+	if remove_http:
+		domain_name = f"{uri.netloc}"
+	else:
+		domain_name = f"{uri.netloc}://{uri.netloc}"
+	return domain_name
+
+
+# Pembuatan Folder Output
+def folder(website):
+	outpath = website
+	if not os.path.exists(outpath):
+		os.makedirs(outpath)
+		print(("## Folder terbuat : " + outpath))
+	return outpath
+
+
+# Pengecekan TOR Service 
+def checktor():
+	checkfortor = subprocess.check_output(['ps', '-e'])
+
+	def findwholeword(w):
+		return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+	if findwholeword('tor')(str(checkfortor)):
+		print("## Layanan TOR telah berjalan!")
+	else:
+		print("## Layanan TOR BELUM berjalan!")
+		print('## Aktifkan tor menggunakan perintah \'service tor start\'')
+		sys.exit(2)
+
