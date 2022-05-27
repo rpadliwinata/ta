f6aa74670b03cf11992982f7cb399fa555369ffb
FinaFahrani <finafahrani@student.telkomuniversity.ac.id>
2022-05-27 14:33:27 +0700

    p

--- v2/main.py
+++ v2/main.py
@@ -16,23 +16,26 @@ if __name__ == "__main__":
 
     for x in range(limit):
         link = links[x]
-        save_to_file(link)
-        register(10, link)
+        save_to_file(link)  # crawling URL sebelum login
+        register(10, link)  # 10 jumalh max attempt
+        # login -> nama cookie karena mau ambil cookie
         cookie = get_cookie(10, link)
-        save_to_file(link, depth=i_depth, pause=i_pause, cookie=cookie)
+        save_to_file(link, depth=i_depth, pause=i_pause,
+                     cookie=cookie)  # crawling setelah login
+        # menghapus link yang sama sebelum dan setelah login
         remove_multiple('links.txt')
 
     link_contain_keyword = []
     crawled_links = [link.strip() for link in open('links.txt', 'r')]
-    if len(crawled_links) != 0:
+    if len(crawled_links) != 0:  # proses crawling sebelumnya berhasil berarti jalanin
         print("Keyword checking...")
         for x in range(limit):
             link = links[x]
             session = proxy_session()
             headers = random_header()
-            cookie = get_cookie(10, link)
+            cookie = get_cookie(10, link)  # pengecekan konten dengan cookie
             for y in crawled_links:
-                if y[:60] == link[:60]:
+                if y[:60] == link[:60]:  # ngecek base URL biar pake cookie yang sama
                     if is_contain_keyword(y, k_word, session, cookie):
                         print(f"{y} is contain '{k_word}'")
                         link_contain_keyword.append(y)
